4a30429e46ea08e8a18dd946ac9080412b373937
AryanDurge <durgearyan9@gmail.com>
2023-12-09 14:57:56 +0530

    Added all files

---
+++ PROJECT-3(weather App)/javasc.js
@@ -0,0 +1,36 @@
+const apikey = "45ab5df9e24578b335b205d10cc7e2d1";
+
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+// const search nire
+
+const searchBox = document.querySelector(".inout");
+
+// const searchBtn = document.querySelector(".card .search button");
+
+const searchBtn = document.querySelector(".search-button");
+
+// const searchBtn = document.querySelector(".search button");
+
+// const weather_icon = document.querySelector("weather-icon");
+
+
+
+async function checkWeather(city){
+
+    const response = await fetch(apiurl + city +`&appid=${apikey}`);
+
+    var data = await response.json();
+
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+}
+
+
+searchBtn.addEventListner("click",()=>{
+    checkWeather(searchBox.value); 
+});
+
